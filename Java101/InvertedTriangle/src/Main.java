7f37e14fb5d5509bee0556480b8204c06d07b32e
Onur Berk Gunes <obgunes99@gmail.com>
2023-07-18 14:34:05 +0300

    Inverted Triangle

--- Java101/InvertedTriangle/src/Main.java
+++ Java101/InvertedTriangle/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int n, i, j;
+        int i, n, j;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter the number for the base: ");
         n = scanner.nextInt();
