f0908264369bf09cfd6c712f9d7bbfb35eea2745
Onur Berk Gunes <obgunes99@gmail.com>
2023-07-23 13:19:43 +0300

    AdventureGame

---
+++ Java102/AdventureGame/src/locations/River.java
@@ -0,0 +1,10 @@
+package locations;
+
+import player.*;
+import obstacle.*;
+
+public class River extends BattleLocation {
+    public River(Player player) {
+        super(player, "River", new Bear(), "Water", 2, 5);
+    }
+}
